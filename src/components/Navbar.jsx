323e26b72c5e6e0931535281437225f67869c097
Bohdan Rudenko <b.r.rudenko@student.khai.edu>
2024-03-20 15:05:41 +0200

    feat: implemented navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,31 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTableList } from "@fortawesome/free-solid-svg-icons";
 import { NavLink } from "react-router-dom";
+
+import "../styles/navbar.css";
 
 // Сomponent representing the website's navigation bar
 const Navbar = () => {
   return (
     <header>
-      <NavLink className="nav-btn" to="/">
-        Euphoria
-      </NavLink>
       <nav>
-        <NavLink className="nav-btn" to="/todos">
+        <NavLink className="nav-btn nav-btnh logo" to="/">
+          <FontAwesomeIcon icon={faTableList} />
+          Softwork
+        </NavLink>
+        <NavLink className="nav-btn nav-btnh" to="/todos">
           Todo List
         </NavLink>
-        <NavLink className="nav-btn" to="/work-timer">
+        <NavLink className="nav-btn nav-btnh" to="/work-timer">
           Work Timer
         </NavLink>
       </nav>
+      <NavLink className="circle-btn nav-btnh" to="/profile">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+          alt="profile"
+        />
+      </NavLink>
     </header>
   );
 };

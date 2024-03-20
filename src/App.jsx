d8f54cd6e599a6e876615d848988c88ce7253438
Bohdan Rudenko <b.r.rudenko@student.khai.edu>
2024-03-20 15:08:12 +0200

    feat: added Profile page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import { Home, TodoList, WorkTimer } from "./pages";
+import { Home, TodoList, WorkTimer, Profile } from "./pages";
 
 // Main component representing the entire application
 const App = () => {
@@ -14,6 +14,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/todos" element={<TodoList />} />
           <Route path="/work-timer" element={<WorkTimer />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </Router>
     </main>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Home from "./Home";
 import TodoList from "./TodoList";
 import WorkTimer from "./WorkTimer";
+import Profile from "./Profile";
 
 // Export the components for use in other parts of the application
-export { Home, TodoList, WorkTimer };
+export { Home, TodoList, WorkTimer, Profile };
